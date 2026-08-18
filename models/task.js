962f233663a66c2fca7e5e0e9d9cc66c698fad92
Akshyasekar <160143513+Akshyasekar@users.noreply.github.com>
2026-08-18 21:47:33 +0530

    Create task.js

---
+++ models/task.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    title: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String
+    },
+
+    status: {
+        type: String,
+        default: "Pending"
+    }
+
+});
+
+module.exports = mongoose.model("Task", taskSchema);

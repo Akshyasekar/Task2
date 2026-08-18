9b9a4fa592387629813efcb6c7da41cd42f452b7
Akshyasekar <160143513+Akshyasekar@users.noreply.github.com>
2026-08-18 21:49:19 +0530

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+
+function auth(req, res, next) {
+
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Please login"
+        });
+    }
+
+    try {
+
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        req.userId = decoded.userId;
+
+        next();
+
+    } catch (error) {
+
+        res.status(401).json({
+            message: "Invalid token"
+        });
+
+    }
+}
+
+module.exports = auth;
